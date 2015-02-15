d53f5f0e49f9d4bd7fec51531ee46d41c65ba489
Mynamesparta <Mynamesparta@i.ua>
2015-02-15 10:39:39 +0200

    Version 1.1000002
    add walk, run, rotation, jump, double jump

---
+++ Unity/Assets/Object/Scripts/Hash_id.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hash_id : MonoBehaviour 
+{
+	public int bool_is_sit;
+	public int float_speed;
+	public int float_angular_speed;
+	public int int_jump;
+	public int Jump_1;
+	void Awake ()
+	{
+		float_speed = Animator.StringToHash ("Speed");
+
+		bool_is_sit = Animator.StringToHash ("Sit");
+
+		float_angular_speed = Animator.StringToHash ("Angular_speed");
+
+		int_jump = Animator.StringToHash ("Jump");
+
+		Jump_1 = Animator.StringToHash ("Base Layer.Jump_1");
+	}
+}
---
+++ Unity/Assets/Object/Scripts/PlayerMovement.cs
@@ -0,0 +1,153 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	public float turnSmoothing = 1f;   // A smoothing value for turning the player.
+	public float speedDampTime = 0.3f;  // The damping for the speed parameter
+	public float speedDampTimeJump=1f;//0.15f;
+	public float maxSpeed =	8f;
+	public float maxAngularSpeed = 5f;
+	public float maxSpaceTimer = 1f;
+	public Vector3 eulerAngleVelocity = new Vector3(0, 500, 0);
+	
+	
+	private Animator anim;              // Reference to the animator component.
+	private Hash_id hash;               // Reference to the HashIDs.
+	private float speed;
+	private float angular_speed;
+	private float jumpspeed;
+	private float timer;
+	private enum State {Normal,Jump};
+	private State state;
+	
+	void Awake ()
+	{
+		// Setting up the references.
+		anim = GetComponent<Animator>();
+		hash = GameObject.FindGameObjectWithTag(Tags.gameController).GetComponent<Hash_id>();
+		timer = 0;
+		// Set the weight of the shouting layer to 1.
+		//anim.SetLayerWeight(1, 1f);
+	}
+	
+	
+	void FixedUpdate ()
+	{
+		// Cache the inputs.
+		float rot = Input.GetAxis("Rotation");
+		float fow = Input.GetAxis("Forward");
+		if(Input.GetButtonDown ("Jump"))
+		{
+			/*/
+			timer+=Time.deltaTime;
+			if(timer> maxSpaceTimer)
+			{
+			}
+			/*/
+			print("test");
+			if(anim.GetInteger(hash.int_jump)==0)
+				anim.SetInteger(hash.int_jump,1);
+			state=State.Jump;
+		}
+		MovementManagement(rot, fow);
+	}
+	
+	
+	void Update ()
+	{
+		// Cache the attention attracting input.
+		//bool shout = Input.GetButtonDown("Attract");
+	}
+	
+	
+	void MovementManagement (float rot, float fow)
+	{
+		speed = anim.GetFloat (hash.float_speed)* Time.deltaTime;
+		angular_speed=anim.GetFloat (hash.float_angular_speed)* Time.deltaTime;
+		//fow = Mathf.Sign (fow);
+		//rot = Mathf.Sign (rot);
+		switch(state)
+		{
+			case State.Normal:
+			{
+				if (rot != 0f) //
+				{
+					ChangeRotation ();
+					anim.SetFloat (hash.float_angular_speed, rot*maxAngularSpeed, speedDampTime*0.5f, Time.deltaTime);
+				}
+				else
+				{
+					anim.SetFloat (hash.float_angular_speed, 0f, speedDampTime, Time.deltaTime);
+				}
+				if (fow != 0f) 
+				{
+					// ... set the players rotation and set the speed parameter to 5.5f.
+					ChangePosition ();
+					anim.SetFloat (hash.float_speed, fow*maxSpeed, speedDampTime, Time.deltaTime);
+				} 
+				else 
+				{
+					//print(speed+">"+speedDampTime);
+					if(speed!=0f)		
+								ChangePosition ();
+					anim.SetFloat (hash.float_speed, 0f, speedDampTime, Time.deltaTime);
+				}
+				break;
+			}
+			case State.Jump:
+			{
+				anim.SetFloat (hash.float_speed, jumpspeed, speedDampTimeJump, Time.deltaTime);
+				if(speed != 0)
+					ChangePosition();
+				break;
+			}
+		}
+		
+	}
+
+	void ChangePosition ()
+	{
+		//
+		//print ("rigidbody:"+rigidbody.rotation.y);
+		float rot = (rigidbody.rotation.eulerAngles.y)* Mathf.Deg2Rad;
+		rigidbody.MovePosition(new Vector3(rigidbody.position.x+speed*Mathf.Sin(rot) ,
+		                                   rigidbody.position.y,
+		                                   rigidbody.position.z+speed*Mathf.Cos(rot) ));
+		//
+		//print (rigidbody.rotation.y+" "+rot+" "+Mathf.Sin(rot) );
+		//print (rigidbody.rotation.eulerAngles);
+	}
+	void ChangeRotation()
+	{
+		Quaternion deltaRotation = Quaternion.Euler(angular_speed*eulerAngleVelocity * Time.deltaTime);
+		rigidbody.MoveRotation(rigidbody.rotation * deltaRotation);
+		//print (rigidbody.rotation.y);
+	}
+	//======================Jump==================================
+	void setJumpSpeed(float jspeed)
+	{
+		if(anim.GetFloat (hash.float_speed)>1f)
+			jumpspeed = jspeed;
+		if(jspeed==2f)
+			anim.SetFloat(hash.float_speed,2f);
+	}
+	void isSecondJump()
+	{
+		print ("second stage");
+		if (Input.GetButton("Jump")) 
+		{
+			anim.SetFloat (hash.float_speed,1f);
+			anim.SetInteger(hash.int_jump,2);
+		}
+		//Input.getB
+	}
+	void endJump()
+	{
+		state=State.Normal;
+		anim.SetInteger (hash.int_jump, 0);
+	}
+	//=============================================================
+
+
+
+}
